5e9c2a6f12eff99d8b3b87108dfe63ed19889305
thuraiselvan10 <thuraiselvan10@gamil.com>
2022-01-28 14:13:01 +0530

    thurai updated code

--- src/test/java/org/test/main/Sample.java
+++ src/test/java/org/test/main/Sample.java
@@ -3,6 +3,10 @@ package org.test.main;
 public class Sample {
 
 	public static void main(String[] args) {
+	
+		System.out.println("test1");
+		System.out.println("test2");
+		System.out.println("test3");
 		
 	}
 	
